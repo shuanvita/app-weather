adc69772e45f5283cc447ced2aaf5314c91865c0
Andrey Shurygin <andrey.sh@solargroup.team>
2025-09-05 14:15:38 +0300

    Add custom colors and font-family

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup></script>
 
 <template>
-  <div>App weather</div>
+  <main class="bg-primary text-2xl text-white">
+    <div>
+      <button class="cursor-pointer bg-[image:var(--color-gradient-button)]">
+        Сохранить
+      </button>
+    </div>
+  </main>
 </template>
 
 <style scoped></style>
