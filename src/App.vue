332c63338d6b7980af25566fc90503767536d4a3
Andrey Shurygin <andrey.sh@solargroup.team>
2025-09-25 20:11:27 +0300

    Dev

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,27 @@ import Stat from "./components/Stat.vue";
 const getCity = (city) => {
   console.log(city);
 };
+
+const dataStat = [
+  {
+    label: "Влажность",
+    stat: "90%",
+  },
+  {
+    label: "Осадки",
+    stat: "0%",
+  },
+  {
+    label: "Ветер",
+    stat: "3 м/ч",
+  },
+];
 </script>
 
 <template>
   <main class="bg-primary grid min-h-[100vh] place-items-center text-white">
     <div class="bg-secondary rounded-[25px] px-[50px] py-15">
-      <Stat label="Влажность" stat="90%" />
+      <Stat v-for="stat in dataStat" v-bind="stat" :key="stat.name" />
       <CitySelect @select-city="getCity" />
     </div>
   </main>
