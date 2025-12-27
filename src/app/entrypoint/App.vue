de59fb79fb1c75beb1bb0c20e83690f6a6c8c785
Andrey Shurygin <shuanvita@gmail.com>
2025-12-27 19:45:36 +0300

    Dev

--- src/app/entrypoint/App.vue
+++ src/app/entrypoint/App.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-2xl">StartKit</h1>
+  <div class="container">
+    <h1 class="text-2xl">StartKit</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, doloremque dolores eius expedita fugiat
+      incidunt voluptatibus. Ad, architecto consequuntur, doloremque dolores dolorum hic, illum maxime nesciunt placeat
+      rem sequi voluptates!
+    </p>
+  </div>
 </template>
