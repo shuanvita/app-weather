8cd82ac536fc064749b494dcce4af1d27ceba0ca
Andrey Shurygin <shuanvita@gmail.com>
2026-01-03 15:07:15 +0300

    Разработать карточку осадков и восход/заход

--- src/pages/weatherPage/ui/components/DetailsWeather.vue
+++ src/pages/weatherPage/ui/components/DetailsWeather.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import ForecastWeek from '@/pages/weatherPage/ui/components/forecastWeek/ForecastWeek.vue'
 import OverviewCards from '@/pages/weatherPage/ui/components/overviewCards/OverviewCards.vue'
+import PrecipitationChart from '@/pages/weatherPage/ui/components/precipitationChart/PrecipitationChart.vue'
+import SunriseSunset from '@/pages/weatherPage/ui/components/sunriseSunset/SunriseSunset.vue'
 
 const { activeDayIndex } = defineProps({
   activeDayIndex: {
@@ -18,5 +20,9 @@ const emit = defineEmits<{
     <ForecastWeek :activeDayIndex="activeDayIndex" @click-day="emit('click-day', $event)" />
     <h2 class="text-[24px] font-semibold">Today’s Overview</h2>
     <OverviewCards />
+    <div class="grid grid-cols-[556px_1fr] gap-5">
+      <PrecipitationChart />
+      <SunriseSunset />
+    </div>
   </div>
 </template>
